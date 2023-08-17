bf949ca184119352bdf698dfc31a8e803a1ae430
NFM <daniel.olovianniy@gmail.com>
2023-08-17 07:39:40 +0300

    initial commit

---
+++ app/src/main/java/ru/nfm/talkingalarm/presentation/AlarmItemActivity.kt
@@ -0,0 +1,12 @@
+package ru.nfm.talkingalarm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.nfm.talkingalarm.R
+
+class AlarmItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_item)
+    }
+}
